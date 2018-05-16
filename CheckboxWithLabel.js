42bf5bf4d0424d8d17945599d2af395094d20269
Tu Pham <tuanh.pham@piksel.com>
2018-05-16 18:19:35 +0700

    skeleton code

---
+++ CheckboxWithLabel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default class CheckboxWithLabel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isChecked: false };
+  }
+
+  onChange = () => {
+    this.setState({ isChecked: !this.state.isChecked });
+  }
+
+  render() {
+    return (
+      <label>
+        <input
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.onChange}
+        />
+        {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+      </label>
+    );
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Author: Tu Pham
 # A small demo for TDD
+Reference: https://facebook.github.io/jest/docs/en/tutorial-react.html
